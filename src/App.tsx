01fa4525697ff7d06b0d344dbe2d4f9d44848191
Bauyrzhan Taishmanov <taishmanov.b.k@gmail.com>
2025-02-07 22:22:00 +0100

    copied template, running version

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { ConfigProvider } from 'antd';
+import { HelmetProvider } from 'react-helmet-async';
+import deDe from 'antd/lib/locale/de_DE';
+import enUS from 'antd/lib/locale/en_US';
+import GlobalStyle from './styles/GlobalStyle';
+import 'typeface-montserrat';
+import 'typeface-lato';
+import { AppRouter } from './components/router/AppRouter';
+import { useLanguage } from './hooks/useLanguage';
+import { useAutoNightMode } from './hooks/useAutoNightMode';
+import { usePWA } from './hooks/usePWA';
+import { useThemeWatcher } from './hooks/useThemeWatcher';
+import { useAppSelector } from './hooks/reduxHooks';
+import { themeObject } from './styles/themes/themeVariables';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const { language } = useLanguage();
+  const theme = useAppSelector((state) => state.theme.theme);
+
+  usePWA();
+
+  useAutoNightMode();
+
+  useThemeWatcher();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <meta name="theme-color" content={themeObject[theme].primary} />
+      <GlobalStyle />
+      <HelmetProvider>
+        <ConfigProvider locale={language === 'en' ? enUS : deDe}>
+          <AppRouter />
+        </ConfigProvider>
+      </HelmetProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
