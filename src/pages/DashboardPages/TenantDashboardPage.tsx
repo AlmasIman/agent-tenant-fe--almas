5328b74e17580a832fc74a8db16112c6ae2a55bd
Bauyrzhan Taishmanov <taishmanov.b.k@gmail.com>
2025-03-29 12:15:41 +0100

    fake dashboard

--- src/pages/DashboardPages/TenantDashboardPage.tsx
+++ src/pages/DashboardPages/TenantDashboardPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PageTitle } from '@app/components/common/PageTitle/PageTitle';
 import { MapCard } from '@app/components/medical-dashboard/mapCard/MapCard';
@@ -14,14 +14,23 @@ import { BloodScreeningCard } from '@app/components/medical-dashboard/bloodScree
 import { NewsCard } from '@app/components/medical-dashboard/NewsCard/NewsCard';
 import { References } from '@app/components/common/References/References';
 import { useResponsive } from '@app/hooks/useResponsive';
+import { httpApi } from '@app/api/http.api';
+import { DepartmentData } from '../UserManagement/userManagementModels';
 import * as S from './DashboardPage.styles';
 import { BaseRow } from '@app/components/common/BaseRow/BaseRow';
 import { BaseCol } from '@app/components/common/BaseCol/BaseCol';
 
 const TenantDashboardPage: React.FC = () => {
   const { isTablet, isDesktop } = useResponsive();
-
   const { t } = useTranslation();
+
+  const [departmentCount, setDepartmentCount] = useState<number>(0);
+
+  useEffect(() => {
+    httpApi.get<DepartmentData[]>('my/departments/').then(({ data }) => {
+      setDepartmentCount(data.length);
+    });
+  }, []);
 
   const desktopLayout = (
     <BaseRow>
@@ -33,32 +42,12 @@ const TenantDashboardPage: React.FC = () => {
             </BaseRow>
           </BaseCol>
 
-          <BaseCol id="map" span={24}>
-            <MapCard />
-          </BaseCol>
-
-          <BaseCol id="latest-screenings" span={24}>
-            <ScreeningsCard />
-          </BaseCol>
-
           <BaseCol id="treatment-plan" xl={24}>
             <TreatmentCard />
           </BaseCol>
 
-          <BaseCol id="covid" xl={24}>
-            <CovidCard />
-          </BaseCol>
-
           <BaseCol id="activity" xl={24} xxl={12}>
             <ActivityCard />
-          </BaseCol>
-
-          <BaseCol id="health" xl={24} xxl={12}>
-            <HealthCard />
-          </BaseCol>
-
-          <BaseCol id="favorite-doctors" xl={24}>
-            <FavoritesDoctorsCard />
           </BaseCol>
 
           <BaseCol id="news" span={24}>
@@ -69,9 +58,6 @@ const TenantDashboardPage: React.FC = () => {
       </S.LeftSideCol>
 
       <S.RightSideCol xl={8} xxl={7}>
-        <div id="blood-screening">
-          <BloodScreeningCard />
-        </div>
         <S.Space />
         <S.ScrollWrapper id="patient-timeline">
           <PatientResultsCard />
@@ -84,42 +70,8 @@ const TenantDashboardPage: React.FC = () => {
     <BaseRow gutter={[20, 20]}>
       <StatisticsCards />
 
-      {isTablet && (
-        <BaseCol id="map" md={24} order={4}>
-          <MapCard />
-        </BaseCol>
-      )}
-
-      <BaseCol id="latest-screenings" xs={24} md={12} order={(isTablet && 5) || 0}>
-        <ScreeningsCard />
-      </BaseCol>
-
       <BaseCol id="activity" xs={24} md={12} order={(isTablet && 8) || 0}>
         <ActivityCard />
-      </BaseCol>
-
-      <BaseCol id="treatment-plan" xs={24} md={24} order={(isTablet && 10) || 0}>
-        <TreatmentCard />
-      </BaseCol>
-
-      <BaseCol id="health" xs={24} md={12} order={(isTablet && 9) || 0}>
-        <HealthCard />
-      </BaseCol>
-
-      <BaseCol id="patient-timeline" xs={24} md={12} order={(isTablet && 11) || 0}>
-        <PatientResultsCard />
-      </BaseCol>
-
-      <BaseCol id="blood-screening" xs={24} md={12} order={(isTablet && 6) || 0}>
-        <BloodScreeningCard />
-      </BaseCol>
-
-      <BaseCol id="favorite-doctors" xs={24} md={24} order={(isTablet && 13) || 0}>
-        <FavoritesDoctorsCard />
-      </BaseCol>
-
-      <BaseCol id="covid" xs={24} md={12} order={(isTablet && 12) || 0}>
-        <CovidCard />
       </BaseCol>
 
       <BaseCol id="news" xs={24} md={24} order={(isTablet && 14) || 0}>
@@ -130,7 +82,7 @@ const TenantDashboardPage: React.FC = () => {
 
   return (
     <>
-      <PageTitle>{t('common.medical-dashboard')}</PageTitle>
+      <PageTitle>Моя компания</PageTitle>
       {isDesktop ? desktopLayout : mobileAndTabletLayout}
     </>
   );
